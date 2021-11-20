76a735a99043efd5cf35a35c714550ddcdd443bf
Aqlan Nor Azman <aqlanhadi@gmail.com>
2021-11-20 19:26:52 +0800

    client to ws change

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client'
 
 //export const socket = io(`ws://localhost:5000`, { transports : ['websocket'] })
-export const socket = io(`wss://um-toothpoly.herokuapp.com:${ process.env.PORT }`, { transports : ['websocket'] })
+export const socket = io()
--- server/app.js
+++ server/app.js
@@ -17,8 +17,11 @@ const io = new Server(httpServer, {
 
 gameSocket(io)
 
-const port = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
+//const INDEX = 'build/index.html';
 
-httpServer.listen(port, () => {
+//app.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+
+httpServer.listen(PORT, () => {
   console.log(`server listening on port ${port}`)
 })
