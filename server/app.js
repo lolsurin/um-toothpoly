06c057fa62b24750fa3a87f35be1485bfea51ba2
Aqlan Nor Azman <aqlanhadi@gmail.com>
2021-11-20 19:30:30 +0800

    fixed typo

--- server/app.js
+++ server/app.js
@@ -23,5 +23,5 @@ const PORT = process.env.PORT || 5000
 //app.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
 
 httpServer.listen(PORT, () => {
-  console.log(`server listening on port ${port}`)
+  console.log(`server listening on port ${PORT}`)
 })
