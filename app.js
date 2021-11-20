f01b02962ae9a75985e48f3d4424264cb363c190
Aqlan Nor Azman <aqlanhadi@gmail.com>
2021-11-20 19:59:57 +0800

    changed structure for postbuild

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const { Server } = require('socket.io')
+const gameSocket = require('./socket')
+
+const app = express()
+
+app.use(express.static('build'))
+
+const httpServer = require('http').createServer(app)
+
+const io = new Server(httpServer, {
+  // cors: {
+  //   origin: ["ws://localhost:3000"],
+  //   methods: ["GET", "POST"]
+  // }
+})
+
+gameSocket(io)
+
+const PORT = process.env.PORT || 5000
+//const INDEX = 'build/index.html';
+
+//app.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+
+httpServer.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`)
+})
