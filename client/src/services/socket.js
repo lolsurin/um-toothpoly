f113e209e01ca268d251c1663156e0426a17136c
Aqlan Nor Azman <aqlanhadi@gmail.com>
2021-11-20 18:49:40 +0800

    typo in env

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,3 +1,3 @@
 import io from 'socket.io-client'
 
-export const socket = io(`ws://${ process.en.NODE_ENV === 'production' ? "um-toothpoly.herokuapp.com:" + process.env.PORT : "localhost:5000"})`, { transports : ['websocket'] })
+export const socket = io(`ws://${ process.env.NODE_ENV === 'production' ? "um-toothpoly.herokuapp.com:" + process.env.PORT : "localhost:5000"})`, { transports : ['websocket'] })
