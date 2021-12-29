cae87dc8f1f51fe4ff09fb9b9e41fae70cb944be
Aqlan Nor Azman <aqlanhadi@gmail.com>
2021-12-30 02:03:47 +0800

    initialized game state

--- events/startGame.js
+++ events/startGame.js
@@ -1,6 +1,24 @@
+const states = require("../states")
+
 module.exports = function(socket, client) {
     client.on('start_game', (data) => {
         console.log('[:server] starting game...')
+
+        let room_idx = states.rooms.findIndex(room => room.players.find(player => player.id == client.id)) // find room index
+        let room = states.rooms[room_idx]       
+        
+        room.state = 'game'
+        room.turn = 0
+        room.players = room.players.map(p => ({
+            ...p, 
+            rank: 1, 
+            score: 0,
+            position: 1,
+            is_winner: false,
+        }))
+
+        console.log(room)
+
         socket.to(data.roomCode).emit('start_game', { code: data.roomCode })
     })
 }
