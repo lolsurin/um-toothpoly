702ae84ce374cdfd699124b7cc831c4218362d7d
Aqlan Nor Azman <aqlanhadi@gmail.com>
2021-12-30 02:03:07 +0800

    ref to state

--- states.js
+++ states.js
@@ -1,2 +1,20 @@
 module.exports.rooms = []
 module.exports.clients = {}
+
+// room state
+// {
+//     roomName: 'MZ1SH',
+//     playerCount: 1,
+//     state: 'game',
+//     players: [
+//       {
+//         id: 'ru_mqRfjz819jjEGAAAH',
+//         name: 'qqqq',
+//         rank: 1,
+//         score: 0,
+//         position: 1,
+//         is_winner: false
+//       }
+//     ],
+//     turn: 0
+//   }
