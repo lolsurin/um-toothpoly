ec3372c87e88c005a7c516e758a213e95aa603d0
Aqlan Nor Azman <aqlanhadi@gmail.com>
2022-01-01 18:25:50 +0800

    multiplayer stage note

--- states.js
+++ states.js
@@ -5,9 +5,16 @@ module.exports.clients = {}
 // {
 //     roomName: 'MZ1SH',
 //     playerCount: 1,
-//     state: 'game',
+//     state: 'lobby|game|question|end',
 //     players: [
 //       {
+//         id: 'ru_mqRfjz819jjEGAAAH',
+//         name: 'qqqq',
+//         rank: 1,
+//         score: 0,
+//         position: 1,
+//         is_winner: false
+//       },{
 //         id: 'ru_mqRfjz819jjEGAAAH',
 //         name: 'qqqq',
 //         rank: 1,
