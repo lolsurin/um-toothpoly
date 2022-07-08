801d0beeb29081285e74b08774aa959e59342ba4
Aqlan Nor Azman <aqlanhadi@gmail.com>
2022-07-08 16:38:13 +0800

    enhancements

--- client/src/features/Layout.js
+++ client/src/features/Layout.js
@@ -5,20 +5,26 @@ function Layout() {
 	const navigate = useNavigate()
 	
 	return (
-		<div className='flex flex-col w-screen h-full min-h-screen gap-2 p-4 m-auto max-w-screen-2xl place-content-between min-w-fit '>
-			<header className='relative flex items-center h-max'>
-				<img src='/um-lg.png' alt='Um Toothpoly' className='h-12' />
-				<span className='invisible mx-4 text-xs font-bold text-gray-700 sm:visible sm:text-xl' onClick={() => navigate('/')}>
-					UNIVERSITI MALAYA TOOTHPOLY 🦷
-				</span>
+		<div className='flex flex-col w-screen h-full min-h-screen gap-2 p-4 m-auto max-w-screen-2xl place-content-between min-w-fit'>
+			<header className='relative flex items-center h-max justify-around'>
+				<img src='/toothpoly.png' alt='Um Toothpoly' className='h-20 mt-4' />
+				{/* <span className='invisible ml-4 text-xs font-bold text-gray-700 sm:visible sm:text-xl' onClick={() => navigate('/')}>
+					TOOTHPOLY 🦷
+				</span> */}
+				{/* <sup>&nbsp;Web Edition</sup> */}
 			</header>
 
 			<div className='relative flex w-full h-full flex-1'>
 				<Outlet />
 			</div>
 
-			<footer className='flex text-slate-400'>
-				&copy; {new Date().getFullYear()} UM
+			<footer className='flex flex-col text-slate-400 leading-tight font-jakarta text-sm justify-around gap-2'>
+				<div className='m-auto'>
+				&copy; {new Date().getFullYear()}&nbsp;Universiti Malaya 
+				</div>
+				<div className='m-auto text-center'>
+				This online game was developed by a group of researcher from Universiti Malaya and the project was funded by the Protoype Research Grant Scheme (PR002-2019B), Ministry of Higher Education, Malaysia.
+				</div>
 			</footer>
 		</div>
 	)
