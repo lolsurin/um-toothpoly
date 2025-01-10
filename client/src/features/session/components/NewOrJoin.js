97b98dc2bf6c460304d7cad60cd0c0e44c8f19a5
Surin Pubalan <77199393+lolsurin@users.noreply.github.com>
2025-01-11 01:21:36 +0800

    test

--- client/src/features/session/components/NewOrJoin.js
+++ client/src/features/session/components/NewOrJoin.js
@@ -76,7 +76,7 @@ function NewOrJoin() {
 					<button
 						type='submit'
 						onClick={(e) => handleNewGame(e)}
-						className='h-24 px-4 py-2 text-2xl font-bold text-white uppercase bg-primary-color rounded-lg w-96 hover:bg-primary-color-800 font-jua'
+						className='h-24 px-4 py-2 text-2xl font-bold text-white uppercase bg-primary-color rounded-lg w-96 hover:bg-primary-color-800'
 					>
 						🦷 Start New Game
 					</button>
